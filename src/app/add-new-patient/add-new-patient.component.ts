6c010f51a7ce55438f53485e313d02d309b8c95a
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-03-18 23:59:39 +0100

    Finish validation for Date of birth and Phone Number; Add validation for Calendar field

--- src/app/add-new-patient/add-new-patient.component.ts
+++ src/app/add-new-patient/add-new-patient.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, MaxValidator } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PersonService } from '../person.service';
-
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-add-new-patient',
   templateUrl: './add-new-patient.component.html',
@@ -10,18 +10,20 @@ import { PersonService } from '../person.service';
 export class AddNewPatientComponent implements OnInit {
 
   basicData: FormGroup;
+  model!: NgbDateStruct;
 
   constructor(private personService: PersonService) { 
     this.basicData = new FormGroup ({
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
-      dateOfBirth: new FormControl('', [Validators.required, Validators.pattern('((((\\b[1-9]\\b|1[0-9]|2[0-8])[-]([1-9]|1[0-2]))|((29|30|31)[-]([13578]|1[02]))|((29|30)[-]([469]|11)))[-]([0-9][0-9][0-9][0-9]))|(29[-]2[-](([0-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)))')]),
-      phoneNumber: new FormControl(''),
+      dateOfBirth: new FormControl('', [Validators.required]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(10)]),
     },
     { updateOn: "blur"});
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+  }
 
   onSubmit() {
     if(this.basicData.valid){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,7 +21,8 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     NgbModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
