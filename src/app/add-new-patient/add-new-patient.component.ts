1d738f432cda09d62e73e455d675047c6dd12cf5
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-03-18 20:38:30 +0100

    full validation for input 'last name' added.

--- src/app/add-new-patient/add-new-patient.component.ts
+++ src/app/add-new-patient/add-new-patient.component.ts
@@ -14,10 +14,11 @@ export class AddNewPatientComponent implements OnInit {
   constructor(private personService: PersonService) { 
     this.basicData = new FormGroup ({
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
-      lastName: new FormControl(''),
-      dateOfBirth: new FormControl(''),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
+      dateOfBirth: new FormControl('', [Validators.required, Validators.pattern('((((\\b[1-9]\\b|1[0-9]|2[0-8])[-]([1-9]|1[0-2]))|((29|30|31)[-]([13578]|1[02]))|((29|30)[-]([469]|11)))[-]([0-9][0-9][0-9][0-9]))|(29[-]2[-](([0-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)))')]),
       phoneNumber: new FormControl(''),
-    });
+    },
+    { updateOn: "blur"});
   }
 
   ngOnInit(): void { }
