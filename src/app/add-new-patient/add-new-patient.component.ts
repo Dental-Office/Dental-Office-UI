2af68703bebc3682c28d2d8ca0b45c900949f900
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-03-21 18:18:55 +0100

    fixed bug when error message is shown too early on Calendar

--- src/app/add-new-patient/add-new-patient.component.ts
+++ src/app/add-new-patient/add-new-patient.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PersonService } from '../person.service';
-import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-add-new-patient',
   templateUrl: './add-new-patient.component.html',
@@ -10,14 +9,14 @@ import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 export class AddNewPatientComponent implements OnInit {
 
   basicData: FormGroup;
-  model!: NgbDateStruct;
-
+  calendarClosed = false;
+  
   constructor(private personService: PersonService) { 
     this.basicData = new FormGroup ({
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
       dateOfBirth: new FormControl('', [Validators.required]),
-      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(10)]),
+      phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(15)]),
     },
     { updateOn: "blur"});
   }
@@ -30,8 +29,10 @@ export class AddNewPatientComponent implements OnInit {
       alert('Form submitted succesfully!')
       const personObservable = this.personService.postPerson(this.basicData.value);
       personObservable.subscribe();
-      console.warn(this.basicData.value);
     }
-  
+  }
+
+  onCalendarClosed() {
+    this.calendarClosed = true;
   }
 }
