39ab62b26b110aaffe9ed0170d818bf2142100e1
Dragana Spasojevic <spasojevicdragana1989@gmail.com>
2022-03-30 11:17:33 +0200

    some changes in naming of component (Person -> Patient)

--- src/app/Patient.ts
+++ src/app/Patient.ts
@@ -1,4 +1,4 @@
-export interface Person {
+export interface Patient {
     id?: number;
     firstName: string;
     lastName: string;
--- src/app/add-new-patient/add-new-patient.component.ts
+++ src/app/add-new-patient/add-new-patient.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { PersonService } from '../person.service';
+import { Patient } from '../Patient';
+import { PatientService } from '../patient.service';
 @Component({
   selector: 'app-add-new-patient',
   templateUrl: './add-new-patient.component.html',
@@ -10,25 +11,35 @@ export class AddNewPatientComponent implements OnInit {
 
   basicData: FormGroup;
   calendarClosed = false;
-  
-  constructor(private personService: PersonService) { 
-    this.basicData = new FormGroup ({
+
+  constructor(private patientService: PatientService) {
+    this.basicData = new FormGroup({
       firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z]*')]),
       dateOfBirth: new FormControl('', [Validators.required]),
       phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(15)]),
     },
-    { updateOn: "blur"});
+      { updateOn: "blur" });
   }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
   }
 
   onSubmit() {
-    if(this.basicData.valid){
-      alert('Form submitted succesfully!')
-      const personObservable = this.personService.postPerson(this.basicData.value);
-      personObservable.subscribe();
+    if (this.basicData.valid) {
+
+      const patientToBeSaved: Patient = {
+        ...this.basicData.value,
+        dateOfBirth: this.basicData.value.dateOfBirth.year + "-" + this.basicData.value.dateOfBirth.month + "-" + this.basicData.value.dateOfBirth.day
+      }
+      this.patientService.postPatient(patientToBeSaved).subscribe({
+        next: () => {
+          alert('Patient successfully saved!')
+        },
+        error: () => {
+          alert('Patiet is not saved!')
+        }
+      });
     }
   }
 
